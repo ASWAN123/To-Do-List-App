819b054124f1a79f06e8b6ccbd9043ac0cc956bb
aswan aswan <aaswan048@gmail.com>
2022-12-19 16:52:34 +0000

    second commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 class Todo{
-    constructor (check , text ){
+    constructor (check , text , id ){
         this.check = check ;
         this.text = text ;
+        this.id = id ;
     }
 }
 
@@ -12,9 +13,10 @@ class UI {
         let destination = document.querySelector(".mylist")
         let row = document.createElement("div")
         row.className = "task" ;
+        row.draggable = true ;
         row.innerHTML = `
             <input class = "done" type="checkbox" ${todo.check == true ? "checked" : "" }>
-            <p>${todo.text}</p>
+            <p id = "${todo.id}">${todo.text}</p>
             <div class="delete"><span> </span><span> </span><span> </span></div>
         `
         let todos = store.data()
@@ -34,13 +36,12 @@ class UI {
 
 
     static removeTask(ele) {
-        ele.parentElement.remove() ;
+        ele.remove() ;
     }
 
 
     static updateStatus(position){
-        let myarray = store.data() ;
-        myarray.reverse() ;
+        let myarray = store.data().reverse() ;
         let trueorfalse = myarray[position].check ;
         document.querySelectorAll(`.task:nth-of-type(${position+1}) .done`)[0].remove() ;
         let div = document.querySelectorAll(`.task:nth-of-type(${position+1})`)[0] ;
@@ -51,7 +52,7 @@ class UI {
         if (trueorfalse === true){
             input.checked = true ;
         }else{
-            input.checked = false ;
+            input.checked = false;
         }
         div.insertBefore(input , befor) ;
 
@@ -60,6 +61,8 @@ class UI {
 
 
 }
+
+
 
 // class storage 
 class store {
@@ -75,7 +78,7 @@ class store {
     }
 
     static gettodos(){
-        let todos = store.data() ;
+        let todos = store.data();
         todos.forEach((todo) => {
             UI.addtodo(todo) ;
         });
@@ -88,10 +91,10 @@ class store {
         
     }
 
-    static removeTask(todo){
+    static removeTask(id){
         let todos = store.data() ;
         todos.forEach((t , index) =>{
-            if(t.text === todo){
+            if(t.id === id){
                 todos.splice(index , 1) ;
             }
         })
@@ -99,16 +102,14 @@ class store {
         localStorage.setItem("todos" , JSON.stringify(todos)) ;
     }
 
-    static updateStatus(ele){
+    static updateStatus(id, index){
         let todos = store.data().reverse() ;
+        let target = todos[index]
         let position ;
-        todos.forEach((t , index) =>{
-            if(t.text === ele){
-                t.check == true ? t["check"]=false : t["check"]=true ;
-                position = index ;
-
-            }
-        })
+        if(target.id === id){
+            target.check == true ? target["check"]=false : target["check"]=true ;
+            position = index ;
+        }
         todos.reverse()
         localStorage.setItem("todos" , JSON.stringify(todos)) ;
         UI.updateStatus(position) ;
@@ -123,6 +124,7 @@ document.addEventListener("DOMContentLoaded" , () =>{
     // update number of items left
     let filtered = store.data().filter((el)=>{return el.check === false;})
     document.querySelector(".items-left").innerText = filtered.length + " items left" ;
+
 })
 
 // event : addtodo
@@ -131,11 +133,12 @@ document.querySelector(".todo-form").addEventListener('submit' , (e)=>{
     // get values 
     let check = document.querySelector('#complete').checked ;
     let text = document.querySelector("#task").value ;
+    let  id  = Math.random().toString(16).slice(2) ;
     
     if (text !== ""){
 
         // into object
-        let todo = new Todo(check , text) ;
+        let todo = new Todo(check , text , id) ;
         // add to UI
         UI.addtodo(todo) ;
         // store todo
@@ -151,19 +154,31 @@ document.querySelector(".todo-form").addEventListener('submit' , (e)=>{
     
 })
 
-// event : removetodo
+// event : removetodo // mark as completed or  mark as not  yet
 document.querySelector(".mylist").addEventListener("click" , (e)=>{
     e.preventDefault() ;
     
     // delete task 
     if (e.target.parentElement.className === 'delete'){
-        UI.removeTask(e.target.parentElement) ;
-        store.removeTask(e.target.parentElement.previousElementSibling.textContent) ;
+        store.removeTask(e.target.parentElement.previousElementSibling.id) ;
+        UI.removeTask(e.target.parentElement.parentElement) ;
+        let filtered = store.data().filter((el)=>{return el.check === false;})
+        document.querySelector(".items-left").innerText = filtered.length + " items left" ;
+        
     }
 
     // chnage mark as complete status
     if (e.target.className === 'done'){
-        store.updateStatus(e.target.nextElementSibling.textContent) ;
+        e.target.parentElement.classList.add("changing")
+        let tasks = document.querySelectorAll('.task') ;
+        tasks.forEach((ele , index) => {
+            if (ele.classList.contains('changing')) {
+                e.target.parentElement.classList.remove('changing') ;
+                store.updateStatus(e.target.nextElementSibling.id , index);
+            }
+        })
+        
+
         // update number of items left
         let filtered = store.data().filter((el)=>{return el.check === false;})
         document.querySelector(".items-left").innerText = filtered.length + " items left" ;
@@ -204,7 +219,7 @@ document.querySelector(".Completed").addEventListener("click" , ()=>{
 })
 
 
-// event filter Active 
+// event filter Active
 document.querySelector(".Active").addEventListener("click" , ()=>{
     // update UI
     let tasks = document.querySelectorAll(".task")
@@ -218,7 +233,7 @@ document.querySelector(".Active").addEventListener("click" , ()=>{
     })
 })
 
-// event filter All 
+// event filter All
 document.querySelector(".All").addEventListener("click" , ()=>{
     // update UI
     let tasks = document.querySelectorAll(".task")
@@ -233,3 +248,64 @@ document.querySelector(".All").addEventListener("click" , ()=>{
 })
 
 // drag and drop
+document.addEventListener("DOMContentLoaded" , () => {
+    let tasks = document.querySelectorAll('.task')
+
+    tasks.forEach((task) => {
+        
+
+        task.addEventListener("dragstart" , (e) => {
+            if(e.target.classList.contains("task")){
+                e.target.classList.add("dragging") ;
+            }
+        })
+
+        task.addEventListener("dragend" , (e) => {
+            e.preventDefault()
+            if (e.target.classList.contains("dragging")){
+
+                let dragging = document.querySelector('.dragging') ;
+                let place = document.querySelector('.place') ;
+                let source = document.querySelector(".mylist") ;
+
+                source.insertBefore(e.target , place) ;
+                dragging.classList.remove("dragging") ;
+                place.classList.remove("place") ;
+
+                // fixing  th e issue or  reordering  after  drag and  drop
+                let tasks = document.querySelectorAll(".task")
+                let todos = [] ;
+                localStorage.setItem("todos" , JSON.stringify(todos)) ;
+                tasks.forEach((ele) => {
+                    let check = ele.querySelector('.done').checked ;
+                    let text = ele.querySelector("p").innerText ;
+                    let id = ele.querySelector(".task >p").id ;
+
+                    let todo = new Todo(check , text , id) ;
+                    // store todo 
+                    store.storeTodo(todo) ;
+                    
+                })
+            }
+
+
+        })
+
+        task.addEventListener("dragover" , (e) => {
+            e.preventDefault() ;
+            if (e.target.classList.contains("task")){
+                e.target.classList.add("place") ;
+            }
+            
+        })
+
+
+        task.addEventListener("dragleave" , (e) => {
+            e.preventDefault() ;
+            e.target.classList.remove("place") ;
+        })
+
+
+    })
+}) 
+
